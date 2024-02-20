09252ebf3b12a9e0d49dbcd14fe7ff7a1cd8280f
Sameer-space <sameersk1025@gmail.com>
2024-02-20 21:00:53 +0530

    Update userAuthMiddleware.js

--- middlewares/userAuthMiddleware.js
+++ middlewares/userAuthMiddleware.js
@@ -3,20 +3,18 @@ const User = require('../models/userModel');
 
 const verifyUserToken = (userToken, res) => {
   // Extract the token from the authorization header
+  try {
   const tokenParts = userToken.split(' ');
   if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-    res.status(401).json({ error: 'Invalid authorization header format' });
-    return null;
+    return res.status(401).json({ error: 'Invalid authorization header format' });
   }
   const token = tokenParts[1];
 
   // Verify the token
-  try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     return decodedToken;
   } catch (error) {
-    res.status(403).json({ error: 'Unauthorized access' });
-    return null;
+    return res.status(403).json({ error: 'Unauthorized access' });    
   }
 };
 
@@ -24,8 +22,7 @@ const findUser = async (decodedToken, res) => {
   try {
     const user = await User.findById(decodedToken.userId);
     if (!user) {
-      res.status(404).json({ error: 'User not found' });
-      return null;
+      return res.status(404).json({ error: 'User not found' });
     }
     return user;
   } catch (error) {
