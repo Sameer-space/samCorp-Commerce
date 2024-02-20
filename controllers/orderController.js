9d7a056f3c2463eadc37055275d4839a79d14033
Sameer-space <sameersk1025@gmail.com>
2024-02-20 20:24:27 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,7 +1,5 @@
 const Order = require('../models/orderModel');
 const Cart = require('../models/cartModel');
-const Product = require('../models/productModel');
-const User  = require("../models/userModel");
 const Address  = require("../models/addressModel");
 const {verifyUserToken,findUser} = require("../middlewares/userAuthMiddleware")
 
